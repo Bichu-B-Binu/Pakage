081f6d009d8f62fb248ed71caacb9991a24a3698
Bichu-B-Binu <bichubbinu77@gmail.com>
2025-07-16 12:00:35 +0530

    Problem

---
+++ DSA/Sorting.java
@@ -0,0 +1,42 @@
+package DSA;
+
+public class Sorting {
+    public static int [] innerSorting(int arr[], int target) {
+
+      
+
+      for(int i= 0; i < arr.length ; i++){
+
+      
+
+        for(int j= arr.length - 1; j > i; j--){
+        
+
+          if(arr[i] == target){
+
+           int temp = arr[j];
+           arr[j] = arr[i];
+           arr[i] = temp;
+
+
+          }
+
+        }
+      }
+    
+      return arr;
+    }
+
+    public static void main(String[] args) {
+      
+      int [ ] array = {6, 1, 6, 8, 10, 4, 15, 6, 3, 9, 6};
+
+      int target = 6;
+
+      int result[] = innerSorting(array , target);
+      System.out.println("Sorted array:");
+      for(int i = 0; i < result.length; i++) {
+          System.out.println( result[i]);
+      }
+    }
+}
---
+++ DSA/Work.java
@@ -0,0 +1,14 @@
+package DSA;
+
+public class Work {
+
+    public static void main(String[] args) {
+        int [] array = {2, 4, 3, 5};
+
+        System.out.println(array.length);
+
+        for(int i = array.length - 1; i >= 0; i--) {
+            System.out.println(array[i]);
+        }
+    }
+}
